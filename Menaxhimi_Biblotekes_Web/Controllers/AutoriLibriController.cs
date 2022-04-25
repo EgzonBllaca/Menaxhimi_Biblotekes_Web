09c950cdecda6d40afab9feeb755f0aa1eaae2b2
Egzon Bllaca <egzonbllaca88@gmail.com>
2022-04-25 13:27:34 +0200

    Edited AutoriLibri

---
+++ Menaxhimi_Biblotekes_Web/Controllers/AutoriLibriController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Menaxhimi_Biblotekes_Web.Models;
+
+namespace Menaxhimi_Biblotekes_Web.Controllers
+{
+    public class AutoriLibriController : Controller
+    {
+        private readonly BiblotekaDbContext _context;
+
+        public AutoriLibriController(BiblotekaDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: AutoriLibri
+        public async Task<IActionResult> Index()
+        {
+            var biblotekaDbContext = _context.AutoriLibri.Include(a => a.Autori).Include(a => a.Libri);
+            return View(await biblotekaDbContext.ToListAsync());
+        }
+
+        // GET: AutoriLibri/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var autoriLibri = await _context.AutoriLibri
+                .Include(a => a.Autori)
+                .Include(a => a.Libri)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (autoriLibri == null)
+            {
+                return NotFound();
+            }
+
+            return View(autoriLibri);
+        }
+
+        // GET: AutoriLibri/Create
+        public IActionResult Create()
+        {
+            ViewData["AutoriId"] = new SelectList(_context.Autori, "Emri", "Emri");
+            ViewData["LibriId"] = new SelectList(_context.Libri, "Titulli", "Titulli");
+            return View();
+        }
+
+        // POST: AutoriLibri/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,LibriId,AutoriId")] AutoriLibri autoriLibri)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(autoriLibri);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AutoriId"] = new SelectList(_context.Autori, "Id", "Id", autoriLibri.AutoriId);
+            ViewData["LibriId"] = new SelectList(_context.Libri, "Id", "Id", autoriLibri.LibriId);
+            return View(autoriLibri);
+        }
+
+        // GET: AutoriLibri/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var autoriLibri = await _context.AutoriLibri.FindAsync(id);
+            if (autoriLibri == null)
+            {
+                return NotFound();
+            }
+            ViewData["AutoriId"] = new SelectList(_context.Autori, "Id", "Id", autoriLibri.AutoriId);
+            ViewData["LibriId"] = new SelectList(_context.Libri, "Id", "Id", autoriLibri.LibriId);
+            return View(autoriLibri);
+        }
+
+        // POST: AutoriLibri/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,LibriId,AutoriId")] AutoriLibri autoriLibri)
+        {
+            if (id != autoriLibri.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(autoriLibri);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AutoriLibriExists(autoriLibri.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AutoriId"] = new SelectList(_context.Autori, "Id", "Id", autoriLibri.AutoriId);
+            ViewData["LibriId"] = new SelectList(_context.Libri, "Id", "Id", autoriLibri.LibriId);
+            return View(autoriLibri);
+        }
+
+        // GET: AutoriLibri/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var autoriLibri = await _context.AutoriLibri
+                .Include(a => a.Autori)
+                .Include(a => a.Libri)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (autoriLibri == null)
+            {
+                return NotFound();
+            }
+
+            return View(autoriLibri);
+        }
+
+        // POST: AutoriLibri/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var autoriLibri = await _context.AutoriLibri.FindAsync(id);
+            _context.AutoriLibri.Remove(autoriLibri);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AutoriLibriExists(int id)
+        {
+            return _context.AutoriLibri.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Menaxhimi_Biblotekes_Web/Controllers/RoliController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Menaxhimi_Biblotekes.Models;
+using Menaxhimi_Biblotekes_Web.Models;
+
+namespace Menaxhimi_Biblotekes_Web.Controllers
+{
+    public class RoliController : Controller
+    {
+        private readonly BiblotekaDbContext _context;
+
+        public RoliController(BiblotekaDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Roli
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Roli.ToListAsync());
+        }
+
+        // GET: Roli/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var roli = await _context.Roli
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (roli == null)
+            {
+                return NotFound();
+            }
+
+            return View(roli);
+        }
+
+        // GET: Roli/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Roli/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Pershkrimi,IsDeleted,IsActive,CreatedByUserID,CreatedOn,LastUpdatedByUserID,LastUpdatedOn")] Roli roli)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(roli);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(roli);
+        }
+
+        // GET: Roli/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var roli = await _context.Roli.FindAsync(id);
+            if (roli == null)
+            {
+                return NotFound();
+            }
+            return View(roli);
+        }
+
+        // POST: Roli/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Pershkrimi,IsDeleted,IsActive,CreatedByUserID,CreatedOn,LastUpdatedByUserID,LastUpdatedOn")] Roli roli)
+        {
+            if (id != roli.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(roli);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RoliExists(roli.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(roli);
+        }
+
+        // GET: Roli/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var roli = await _context.Roli
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (roli == null)
+            {
+                return NotFound();
+            }
+
+            return View(roli);
+        }
+
+        // POST: Roli/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var roli = await _context.Roli.FindAsync(id);
+            _context.Roli.Remove(roli);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RoliExists(int id)
+        {
+            return _context.Roli.Any(e => e.Id == id);
+        }
+    }
+}
