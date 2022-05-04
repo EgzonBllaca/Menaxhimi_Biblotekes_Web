9c42dc91c0f7dc07b20b87104a1051989eddc156
ShendritB <shendrit-beqa@outlook.com>
2022-05-04 12:19:27 +0200

    Migrations

--- Menaxhimi_Biblotekes_Web/Models/Autori.cs
+++ Menaxhimi_Biblotekes_Web/Models/Autori.cs
@@ -11,13 +11,13 @@ namespace Menaxhimi_Biblotekes.Models
         public int Id { get; set; }
         public string Emri { get; set; }
         public string Mbiemri { get; set; }
-        public bool IsDeleted { get; set; }
+        //public bool IsDeleted { get; set; }
 
-        public bool IsActive { get; set; }
-        public int CreatedByUserID { get; set; }
-        public DateTime CreatedOn { get; set; }
-        public int LastUpdatedByUserID { get; set; }
-        public DateTime LastUpdatedOn { get; set; }
+        //public bool IsActive { get; set; }
+        //public int CreatedByUserID { get; set; }
+        //public DateTime CreatedOn { get; set; }
+        //public int LastUpdatedByUserID { get; set; }
+        //public DateTime LastUpdatedOn { get; set; }
 
         public virtual ICollection<AutoriLibri> AutoriLibri { get; set; }
     }
